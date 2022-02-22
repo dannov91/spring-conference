2922e37211ce94963ca305d14f3f681f5a9e1f74
Daniel Oviedo <doviedo@motus.com>
2022-02-22 15:44:27 -0500

    [CONF-1] tweaking DB config

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -1,19 +1,15 @@
 package com.pluralsight.conferencedemo.config;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
+    /*@Bean
     public DataSource dataSource() {
         DataSourceBuilder builder = DataSourceBuilder.create();
         builder.url("jdbc:postgresql://localhost:5432/conference_app");
         builder.username("postgres");
         builder.password("postgres");
         return builder.build();
-    }
+    }*/
 }
